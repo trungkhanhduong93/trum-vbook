306263d91da5eeef160304963f70e45c6a41d01e
trungkhanhduong93 <trungkhanhduong93@gmail.com>
2026-05-22 19:05:56 +0700

    fix(mangak): return plain array of strings in chap.js and bump version to 2

--- mangak/src/chap.js
+++ mangak/src/chap.js
@@ -8,13 +8,18 @@ function execute(url) {
             imgs = doc.select(".chapter-img-wrapper img");
         }
         var data = [];
+        var seen = {};
         imgs.forEach(e => {
             var link = e.attr("src") || e.attr("data-src") || "";
             if (link) {
+                link = link.trim();
                 if (link.startsWith("//")) {
                     link = "https:" + link;
                 }
-                data.push({link: link});
+                if (!seen[link]) {
+                    seen[link] = true;
+                    data.push(link);
+                }
             }
         });
         return Response.success(data);
