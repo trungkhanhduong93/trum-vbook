3c004983e8337a57ca6152b96dfd8d4e302c28d4
trungkhanhduong93 <trungkhanhduong93@gmail.com>
2026-08-12 20:51:10 +0700

    remove(goctruyentranh): remove GocTruyenTranh plugin and gtt-proxy server

--- goctruyentranh/src/home.js
+++
@@ -1,9 +0,0 @@
-load('config.js');
-
-function execute() {
-    return Response.success([
-        { title: 'Trang chủ (Mới cập nhật)', input: '/api/proxy/v2/search?p=0&orders=updateTime', script: 'gen.js' },
-        { title: 'Truyện Mới', input: '/api/proxy/v2/search?p=0&orders=createdAt', script: 'gen.js' },
-        { title: 'Đang Hot', input: '/api/proxy/v2/search?p=0&orders=viewCount', script: 'gen.js' }
-    ]);
-}
