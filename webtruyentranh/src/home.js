b1bca83513aa504f77bde7919f510b2aa4a31773
trungkhanhduong93 <trungkhanhduong93@gmail.com>
2026-09-03 23:53:54 +0700

    chore: loai bo otruyen va webtruyentranh do may chu backend da sap vinh vien

--- webtruyentranh/src/home.js
+++
@@ -1,8 +0,0 @@
-load("config.js");
-
-function execute() {
-    return Response.success([
-        { title: "Mới Cập Nhật", input: API_BASE + "/danh-sach/truyen-moi",  script: "search.js" },
-        { title: "Hoàn Thành",   input: API_BASE + "/danh-sach/hoan-thanh",  script: "search.js" }
-    ]);
-}
